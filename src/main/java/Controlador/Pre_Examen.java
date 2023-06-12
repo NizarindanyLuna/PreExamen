3cd2b0f0c28474af87e5454ad013d3a089063356
Nizarindany González Luna <nizikyua.18@gmail.com>
2023-06-11 22:45:03 -0600

    "Generacion del proyecto"

---
+++ src/main/java/Controlador/Pre_Examen.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Controlador;
+
+/**
+ *
+ * @author Is Dahud
+ */
+public class Pre_Examen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
